1aaf8506e2604eaa74a8f7e670d3dd845fa869dd
Rahman Çaylak <rahmancaylakk@gmail.com>
2022-05-08 23:25:32 +0300

    Topbar component created.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,15 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import { store } from '../redux/store.js';
+import { Provider } from 'react-redux';
+import Header from '../components/Header';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider store={store}>
+      <Header />
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
