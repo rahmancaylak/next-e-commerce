9ec32fb98ba9399232a3af6ec8e88af6e6c4ce6c
Rahman Çaylak <rahmancaylakk@gmail.com>
2022-05-12 23:21:28 +0300

    Modal component created.

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,12 +1,12 @@
-import React from 'react';
 import TopBar from './TopBar/index.js';
 import Logo from './Logo/index.js';
 import Search from './Search/index.js';
 import WidgetGroup from './WidgetGroup/index.js';
-
+import Modal from './Modal/index.js';
 export default function index() {
   return (
     <header>
+      <Modal />
       <TopBar />
       <section className='mt-3'>
         <div className='container border-b pb-5 mx-auto p-2 flex justify-between items-center'>
